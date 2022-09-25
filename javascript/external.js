93cf27197e15d747de8f765957638de3e5338f67
YaoTingYao <leannayao@gmail.com>
2022-09-25 19:54:22 -0400

    Completed Task 5. Conditional Statement

--- javascript/external.js
+++ javascript/external.js
@@ -8,7 +8,7 @@
 //const out = x + y;
 //document.write(out);
 
-//A prompt box is used to prompt users to input a value before entering a page.
+/*//A prompt box is used to prompt users to input a value before entering a page.
 user_name = window.prompt('Please enter your name', 'Type your name here');
 document.write(user_name);
 
@@ -20,4 +20,25 @@ sign = window.prompt('Are you feeling lucky', 'sure'); // open the window with T
 
 // A prompt box is used to prompt users to input a value before entering a page.
 user_name = window.prompt('Please enter your name', 'Type your name here');
-document.write(user_name);
+document.write(user_name);*/
+
+/*x = 22;
+const y =33;
+document.writeln(x + y)  ;
+document.writeln("<br>");
+document.writeln(x += 33);*/
+
+document.writeln("<button onclick='condition()'>Conditional Test</button>")
+
+function condition()
+{
+  x = confirm("Are you sure you want to proceed?");
+  if(x)
+  {
+    document.writeln("You chose Okay!");
+  }
+  else
+  {
+    document.writeln("You chose Cancel!");
+  }
+}
