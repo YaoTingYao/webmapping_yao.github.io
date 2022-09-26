e5adbf61fea7613bc4ddab3ee4a73bc6dd92baea
YaoTingYao <leannayao@gmail.com>
2022-09-25 22:46:30 -0400

    Replaced the header with user prompt information.

--- javascript/external.js
+++ javascript/external.js
@@ -51,4 +51,3 @@ document.writeln(x += 33);*/
 
 //A prompt box is used to prompt users to input a value before entering a page.
 var user_name = window.prompt('Please enter your name', 'Type your name here');
-document.write(user_name);
