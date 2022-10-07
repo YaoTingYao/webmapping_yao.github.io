0120937891ad50bf6627c3184da3859fe031e4e3
YaoTingYao <leannayao@gmail.com>
2022-10-07 01:41:20 -0400

    Improved web map table aesthetics.

--- javascript/external.js
+++ javascript/external.js
@@ -1,58 +1,3 @@
-//document.write("Where is my JS code?");
-//window.alert(5 + 6);
-//window.alert(5 + 6);
-//document.write('<h1>A heading</h1>');
-//document.write('<p>A sentence.</p>');
-//const x = "web";
-//const y = "mapping";
-//const out = x + y;
-//document.write(out);
-
-/*//A prompt box is used to prompt users to input a value before entering a page.
-user_name = window.prompt('Please enter your name', 'Type your name here');
-document.write(user_name);
-// there are many ways to use the prompt feature
-sign = window.prompt(); // open the blank prompt window
-sign = prompt();       //  open the blank prompt window
-sign = window.prompt('Are you feeling lucky'); // open the window with Text "Are you feeling lucky"
-sign = window.prompt('Are you feeling lucky', 'sure'); // open the window with Text "Are you feeling lucky" and default value "sure"
-// A prompt box is used to prompt users to input a value before entering a page.
-user_name = window.prompt('Please enter your name', 'Type your name here');
-document.write(user_name);*/
-
-/*x = 22;
-const y =33;
-document.writeln(x + y)  ;
-document.writeln("<br>");
-document.writeln(x += 33);*/
-
-// document.writeln("<button onclick='condition()'>Conditional Test</button>")
-//
-// function condition()
-// {
-//   x = confirm("Are you sure you want to proceed?");
-//   if(x)
-//   {
-//     document.writeln("You chose Okay!");
-//   }
-//   else
-//   {
-//     document.writeln("You chose Cancel!");
-//   }
-// }
-
-// arr = ["Joe","April","Mark","Sara"];
-// for (var i=0; i < arr.length; i++)
-// {
-//   document.writeln(arr[i],"<br>");
-// }
-
-//A prompt box is used to prompt users to input a value before entering a page.
-
-//var user_name = window.prompt('Please enter your name', 'Type your name here');
-//document.write('<h1>Hello, Welcome to my webpage, '+user_name +' !</h1>');
-
-
 function welcome()
 
 {
@@ -69,7 +14,6 @@ return message
 
 }
 
-
 var webmaps =
 [
     ["National Risk Index for Natural Hazards",
@@ -83,13 +27,13 @@ var webmaps =
 ];
 
 function webmap_table(){
-    document.write("<table width=100%>")
-    for(var row = 0; row <webmaps.length; row++){
-        document.write("<tr>");
-        for(var column = 0; column <webmaps[0].length; column++){
+	    document.write("<table width=100%>")
+	    for(var row = 0; row <webmaps.length; row++){
+        if (row < 1) {document.write("<tr style='background-color:rgb(204, 156, 172);'>")} else {document.write("<tr style='background-color:rgb(209, 196, 233);'>")};
+	        for(var column = 0; column <webmaps[0].length; column++){
             document.write("<td>"+webmaps[row][column]+"</td>");
-        }
-    document.write('</tr>')
+	        }
+	    document.write('</tr>')
     }
-    document.write('</table>')
-}
+	    document.write('</table>')
+	}
